f209951314b167f107662982015574961597f86a
PKmahto9771 <prankishor0111@gmail.com>
2025-07-24 03:56:44 +0530

    import included

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const USER = require('../models/User')
+const checkAuth = require('../middlewares/auth');
 
 const router = express.Router();
 
